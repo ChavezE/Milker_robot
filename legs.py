89b47a0d4eb17dc5b25a9c573e9659727831d0a3
ChavezE <emilio_10.futbol@hotmail.com>
2016-06-09 20:14:18 -0500

    fixed logic

--- legs.py
+++ legs.py
@@ -3,23 +3,30 @@
 # indicators
 class Mylegs:
 	""" squares here """
-	# variables
-	self.leftLeg = [] # here we store coordenates of left leg
-	self.contLeftLeg = 0 # This will tell us the size of the list 'leftLeg'
-	self.rightLeg = [] # coord for right leg
-	self.contRightLeg = 0 # This will tell us the size of the list 'rightLeg'
-	#
+
+
 	def __init__(self, listaCuadros):
-		# looking for the upper left-est square
-		self.mostLeft = listaCuadros[0]
-		for i in range(0,len(listaCuadros)):
-		 	if (listaCuadros[i][0] <= self.mostLeft[0]) and (listaCuadros[i][1]
-			<= self.mostLeft[1]):
-			# When you find the upper left corner assign it to the atribute
-		 		self.mostLeft = listaCuadros[i]
-		# Assign the value find as the first coordinate of the left Leg
-		self.leftLeg[0] = self.mostLeft
-		self.contLeftLeg += 1
+
+		# variables
+		self.leftLeg = [] # here we store coordenates of left leg
+		self.contLeftLeg = 0 # This will tell us the size of the list 'leftLeg'
+		self.rightLeg = [] # coord for right leg
+		self.contRightLeg = 0 # This will tell us the size of the list 'rightLeg'
+		#
+
+		# looking for left-est square
+		mostLeft = listaCuadros[0] # auxiliar variable 
+		for i in range(0,len(listaCuadros)): # first find the left
+		 	if (listaCuadros[i][0] <= mostLeft[0]):
+		 		mostLeft = listaCuadros[i]
+		for i in range(0,len(listaCuadros)): # now find the upper
+			if( listaCuadros[i][0] <= mostLeft[0] and listaCuadros[i][1] <= mostLeft[1]):
+				mostLeft = listaCuadros[i]
+
+		self.leftLeg.insert(0,mostLeft)
+		self.contLeftLeg = self.contLeftLeg + 1 
+
+
 	def findLeftLeg(self, listaCuadros):
 		# Define 'epsilon' which is the tolerance in px to find the other
 		# coordinates that form the left Leg
@@ -34,4 +41,3 @@ class Mylegs:
 lista = [[2,2],[2,3],[3,1],[7,0],[2,0]]
 
 pata = Mylegs(lista)
-print (pata.mostLeft)

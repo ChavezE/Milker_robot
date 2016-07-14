bbce1277313b78b18c0935f9dbf483f19cb17339
ChavezE <chaaveze@gmail.com>
2016-07-13 19:17:52 -0500

    Fixing some stuff
    # No hay robot

--- rotRec.py
+++ rotRec.py
@@ -3,16 +3,42 @@ import time
 import numpy as np
 
 filename = 'image2.jpg'
-imgOriginal = cv2.imread(filename)
-imgOriginal = cv2.resize(imgOriginal, (720 ,480))
+binValue = 65 # parameter for the threshold
+
+
+# loads image as imgOriginal
+# This will have to change to taking snap everytime needed
+# in order to do this we must initialice cv2.videoCapture in the future
+# and remove parameter filename
+def loadImage(capName):
+	
+	imgOriginal = cv2.imread(capName)
+	imgOriginal = cv2.resize(imgOriginal, (720 ,480))
+	return imgOriginal
+
+#########	This will change when we use videoCapute #######
+imgOriginal = loadImage(filename)
+
+# Does some guassian filtering to remove noise
+def clearImage(imgOriginal):
+	imGray = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2GRAY)
+	imGray = cv2.GaussianBlur(imGray, (3,3), 2)
+	return imGray
+
+def doThresHold(filteredImage,tVal):
+	_, thres1 = cv2.threshold(filteredImage,tVal,255,cv2.THRESH_BINARY_INV)
+	thres1 = cv2.erode(thres1,np.ones((5,5),np.uint8), iterations=2)
+	return thres1
 
 # This method will recibe all countours in image and
 # will make all necesary filtering in order to achive
 # countors with high probability of being cow rectangles
 def getGoodSquares(contours):
 
-	allR = [] # temporal for storing
+	# temporal for storing
+	allR = [] 
 	cowSqrs = [] 
+
 	for cnt in contours:
 		area = cv2.contourArea(cnt)
 		rect = cv2.minAreaRect(cnt)
@@ -29,7 +55,9 @@ def getGoodSquares(contours):
 			if (extent >= 0.7):   # tolerance
 				allR.insert(0,cnt)
 				cowSqrs.insert(0,[area,extent])
-        cv2.drawContours(imgOriginal, allR, -1, (0,255,0))
+	cv2.drawContours(imgOriginal, allR, -1, (0,255,0))
+	
+	'''
 	# FILTER SQUARES FOR ITS AREA
 	(allR, cowSqrs) = zip(*sorted(zip(allR, cowSqrs),
 		key=lambda b:b[1][0], reverse=False))
@@ -60,7 +88,7 @@ def getGoodSquares(contours):
 
 	# FILTER squares for its POSITION on the image
 	# STILL NOT WORKING
-	'''
+	
 	for cnt in allR:
 		x,y,w,h = cv2.boundingRect(cnt)
 		# First we assume that 'cnt' has no neighbours neither in x nor in y axis
@@ -85,7 +113,6 @@ def getGoodSquares(contours):
 	'''
 	
 
-                                                
 	return allR
 
 # This function is still on progress, hope to return well ordered body of cow
@@ -124,15 +151,14 @@ def boundingRectSort(allRect,criteria):
 ################################################
 def loop():
 
-	binValue = 65 # parameter for the threshold
-	##########################################################################################
-	imGray = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2GRAY)
-	imGray = cv2.GaussianBlur(imGray, (3,3), 2)
-	_, thres1 = cv2.threshold(imGray,binValue,255,cv2.THRESH_BINARY_INV)
-	thres1 = cv2.erode(thres1,np.ones((5,5),np.uint8), iterations=2)
-	cv2.imshow('t',thres1)
-	image, contours, hierarchy = cv2.findContours(thres1,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
-	##########################################################################################
+
+	filteredImage = clearImage(imgOriginal)
+	thresImage = doThresHold(filteredImage,binValue)
+	cv2.imshow('T',thresImage)
+
+	# Getting here (and other thing we're not using)
+	image, contours, hierarchy = cv2.findContours(thresImage,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+
 
 	allRect = getGoodSquares(contours)
 	boundingBoxes = [] # will be used in parallel with allRect
@@ -140,9 +166,8 @@ def loop():
 	""" At this point we have all posible squares filtered and stored  AS COUNTOURS in allRect """
 
 	font = cv2.FONT_HERSHEY_SIMPLEX # This line defines the font 
-
-        cv2.drawContours(imgOriginal,allRect,-1,(0,255,0),1)
-        """
+	cv2.drawContours(imgOriginal,allRect,-1,(0,255,0),1)
+	"""
 	for cnt in range(0,len(allRect)):
 		x = boundingBoxes[cnt][0]
 		y = boundingBoxes[cnt][1]

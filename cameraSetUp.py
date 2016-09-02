4c90a8d3ad80feef937c2930b4fe48be7756143c
sebasrivera96 <sebas.rivera96@gmail.com>
2016-09-02 15:49:51 -0500

    Building up a code to setup the camera

---
+++ cameraSetUp.py
@@ -0,0 +1,24 @@
+'''
+Created by: EMIBAS		01/09/2016
+Version 1.0
+
+Objective: This program needs to be runned every time the camera is connected to the RaspberryPi.
+The main purpose is to calibrate some commands that are necessary in order to take useful pictures
+when searching for the cow. The command that is used is the 'uvcdynctrl'.
+'''
+import gtk
+import subprocess
+
+UVCDYNCTRLEXEC = "/usr/bin/uvcdynctrl"
+
+def initializeCamera():
+	subprocess.Popen([UVCDYNCTRLEXEC,"-s",'Pan Reset',"--",'1'])
+	subprocess.Popen([UVCDYNCTRLEXEC,"-s",'Tilt Reset',"--",'1'])
+	subprocess.Popen([UVCDYNCTRLEXEC,"-s",'Backlight Compensation',"--",'0'])
+	subprocess.Popen([UVCDYNCTRLEXEC,"-s",'Exposure, Auto',"--",'1'])
+	subprocess.Popen([UVCDYNCTRLEXEC,"-s",'White Balance Temperature, Auto',"--",'0'])
+	subprocess.Popen([UVCDYNCTRLEXEC,"-s",'Focus',"--",'0'])
+	subprocess.Popen([UVCDYNCTRLEXEC,"-s",'Gain',"--",'30'])
+
+
+initializeCamera()
